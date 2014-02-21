080fdcc6e1974a6f1ef2d728c92d6df96e9da6c1
dpbergquist <dpbergquist@gmail.com>
2014-02-21 23:38:23 +0000

    ex5 additional work complete

--- ex5.rb
+++ ex5.rb
@@ -1,17 +1,19 @@
-my_name = 'Zed A. Shaw'
-my_age = 35 #not a lie
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 #not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+cm_to_1in = 2.54
+kg_to_1lb = 0.453592
 
-puts "Let's talk about %s." % my_name
-puts "He's %d inches tall." % my_height
-puts "He's %d pounds heavy." %my_weight
+puts "Let's talk about %s." % name
+puts "He's %d inches (or %d cm) tall." % [height, height * cm_to_1in]
+puts "He's %d pounds (or %d kg) heavy." % [weight, weight * kg_to_1lb]
 puts "Actually that's not too heavy."
-puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
-puts "His teeth are usually %s depending on the coffee." % my_teeth
+puts "He's got %s eyes and %s hair." % [eyes, hair]
+puts "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
-puts "If I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age + my_height + my_weight]
+puts "If I add %d, %d, and %d I get %d." % [age, height, weight, age + height + weight]
