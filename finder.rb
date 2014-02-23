a9c0d273973f717e66b03403931f4950d4d14e62
dpbergquist <dpbergquist@gmail.com>
2014-02-23 16:07:50 +0000

    found all the hidden numbers!

--- finder.rb
+++ finder.rb
@@ -5,47 +5,61 @@
 # needs: a counter that increases for each "guess" the program makes
 # needs: a function that makes the "guess" by choosing a number in the centre of the range
 # needs: a function to check whether the hidden number is higher or lower than the "guess" and reduces the range accordingly
-# needs: a hash to store the results - using the numbers of the range as keys and the number of guesses to reach each number as the value
+# needs: a hash to store the results - using the numbers of the range as keys and the number of div_guesses to reach each number as the value
 
-puts "This program will attempt to discover the fastest method of finding a hidden number within a range."
-puts "Please enter the highest number in the range:"
-range_high = gets.chomp.to_i
+range_high = 100
 
-puts "Please enter the lowest number in the range:"
-range_low = gets.chomp.to_i
+range_low = 1
 
-the_range = Array (range_low..range_high)
+the_range = Array (range_low..range_high) 
 
-current_range = the_range
+hidden_number = range_low #this sets the number we have to find - maybe try to put this inside the hash, right from the start?
 
-hidden_number = Array (range_low..range_high)
-
-guesses = Hash.new {}
-
-def midpoint(a)
+def midpoint(a) #this makes a guess in the middle of the range of possibilities
 	len = a.length
 	a[(len -1) / 2]
 end
 
+def gold_ratio(a)
+	len = a.length
+	a[(len-1) / 1.62]
 
-hidden_number.each do |x|
-	guess = midpoint(current_range)
-	guess_count = 1
-if guess < x 
-	guess_count += 1 
-	current_range.keep_if { |v| v > guess}
-elsif guess > x
+def is_correct(g, x)
+	g == x
+end
+
+def is_higher(g, x)
+	g > x
+end
+
+def is_lower(g, x)
+	g < x
+end
+
+guess_count = 0
+
+div_guesses = Hash.new {} #this is where we will store the results 
+
+until hidden_number == range_high
+	guess = midpoint(the_range)
 	guess_count += 1
-	current_range.keep_if { |v| v < guess}
-elsif guess == x
-	guesses[x] = guess_count
-	current_range = the_range
+	if is_higher(guess, hidden_number)
+		the_range.keep_if {|n| n <= guess}
+	elsif is_lower(guess, hidden_number)
+		the_range.keep_if {|n| n >= guess}
+	else div_guesses[hidden_number] = guess_count
+		the_range = Array (range_low..range_high)
+		guess_count = 0
+		hidden_number += 1
+	end
 end
 
-end
+div_guesses.each {|x, y| puts "Hidden number: #{x} Guesses: #{y}"}
 
 
-guesses.each {|x, y| puts "Hidden number: #{x} Guesses: #{y}"}
+
+
+
 
 
 
